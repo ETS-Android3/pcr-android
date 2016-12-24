e26e260251ec5c889122e31e6cc6ba40d3882869
Vikas Peddu <vikas.peddu@gmail.com>
2016-12-23 17:30:19 -0800

    adding variables

--- PcrApp/app/src/main/java/com/example/vishaalprasad/pcrapp/MainActivity.java
+++ PcrApp/app/src/main/java/com/example/vishaalprasad/pcrapp/MainActivity.java
@@ -5,6 +5,19 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int dntp0;
+    private int fprimer0;
+    private int rprimer0;
+    private int pol0;
+    private int template;
+    private int water;
+    private int rxnvolume;
+    private int	dntp1;
+    private int	fprimer1;
+    private int	rprimer1;
+    private int	pol1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
