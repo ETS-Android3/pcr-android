143f46d0edf937b40579fcf8fae4a295073ee5bd
Vishaal Prasad <vishaal56@gmail.com>
2017-11-18 13:41:08 -0800

    updating gradle, android studio version, using kotlin for PceResult

---
+++ PcrApp/app/src/main/java/com/example/vishaalprasad/pcrapp/reactant_helpers/PcrResult.kt
@@ -0,0 +1,13 @@
+package com.example.vishaalprasad.pcrapp.reactant_helpers
+
+import java.io.Serializable
+
+/**
+ * Holds each PCR Reuslt Reactant
+ */
+class PcrResult : Serializable {
+
+    var name: String = ""
+    var perTube: Double = 0.0
+
+}
